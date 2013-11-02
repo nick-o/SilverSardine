ccad45219f8fb05a17fdd6b7fff8ee0c64afdc1e
Nico Engelen <engelen.nico@googlemail.com>
2013-11-02 11:26:52 +0000

    Fixing problem with redploying Service

---
+++ source/SilverSardine.WindowsService/PreDeploy.ps1
@@ -0,0 +1,15 @@
+﻿$ServiceName = "SilverSardine"
+
+$service = Get-Service $ServiceName -ErrorAction SilentlyContinue
+
+
+if (! $service)
+{
+    Write-Host "The service is not yet installed. Nothing to do."
+}
+else
+{
+    Write-Host "The service will be stopped."
+
+    Stop-Service $ServiceName -Force
+}
